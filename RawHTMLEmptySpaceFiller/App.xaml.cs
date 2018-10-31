d68bc752f268330cbc0b222b3a1c6acecf88f7d7
grigdevelop <grigdevelop@gmail.com>
2018-10-31 18:57:49 +0100

    initial commit

---
+++ RawHTMLEmptySpaceFiller/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace RawHTMLEmptySpaceFiller
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ RawHTMLEmptySpaceFiller/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace RawHTMLEmptySpaceFiller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string myText = new TextRange(InputTextBox.Document.ContentStart, InputTextBox.Document.ContentEnd).Text;
+            string[] lines = myText.Split(new[] { "\n" }, StringSplitOptions.None);
+            StringBuilder sb = new StringBuilder();
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    sb.AppendLine(line);
+                    continue;
+                }
+                var nIndex = getIndexOfNonEmpty(line);
+                if (nIndex == -1)
+                {
+                    sb.AppendLine(line);
+                    continue;
+                }
+
+                string emptyPart = line.Substring(0, nIndex);
+                string newString = "<span style=\"color: #569cd6;\">" + emptyPart + @"</span>";
+                sb.AppendLine(newString + line.Substring(nIndex + 1));
+            }
+
+            string finalString = sb.ToString();
+
+            InputTextBox.Document.Blocks.Clear();
+            InputTextBox.Document.Blocks.Add(new Paragraph(new Run(finalString)));
+        }
+
+        private int getIndexOfNonEmpty(string input)
+        {
+            int index = -1;
+            int i = 0;
+            while (input[i] == ' ' && i < input.Length - 1)
+            {
+                index = i;
+                i++;                
+            }
+
+            return index;
+        }
+
+    }
+}
